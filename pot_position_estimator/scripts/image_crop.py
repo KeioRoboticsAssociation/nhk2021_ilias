16ab6fe134927da0025e299a301a78948c9bfeb4
Decwest <fumiyaonishi1016@gmail.com>
2021-03-31 22:15:54 +0900

    FIX : save depth image as npy format

--- pot_position_estimator/scripts/image_crop.py
+++ pot_position_estimator/scripts/image_crop.py
@@ -11,10 +11,12 @@ import datetime
 class ImageCrop:
     def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber(
-            "/camera/depth/image_raw", Image, self.image_callback, queue_size=1)
+        self.depth_sub = rospy.Subscriber(
+            "/camera/depth/image_raw", Image, self.depth_callback, queue_size=1)
+        self.color_sub = rospy.Subscriber(
+            "/camera/color/image_raw", Image, self.color_callback, queue_size=1)
 
-    def image_callback(self, ros_image):
+    def depth_callback(self, ros_image):
         try:
             frame = self.bridge.imgmsg_to_cv2(ros_image, 'passthrough')
         except CvBridgeError, e:
@@ -24,11 +26,31 @@ class ImageCrop:
         date_time = dt_now.isoformat().replace(':', '-')
         filename = "depth_image.png"
         filepath = "../data/" + date_time + "_" + filename
-        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.imwrite(filepath, frame)
+
+        filename_npy = "depth_image.npy"
+        filepath = "../data/" + date_time + "_" + filename_npy
+        depth_array = np.array(frame, dtype=np.float32)
+        np.save(filepath, depth_array)
+
         print("create "+ filepath)
         #cv2.waitKey(1)
 
+    def color_callback(self, ros_image):
+        try:
+            frame = self.bridge.imgmsg_to_cv2(ros_image, 'passthrough')
+        except CvBridgeError, e:
+            rospy.logerr(e)
+
+        dt_now = datetime.datetime.now()
+        date_time = dt_now.isoformat().replace(':', '-')
+        filename = "color_image.png"
+        filepath = "../data/" + date_time + "_" + filename
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(filepath, frame)
+
+        print("create " + filepath)
+        #cv2.waitKey(1)
 
 if __name__ == '__main__':
     try:
